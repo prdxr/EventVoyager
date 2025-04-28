6d551400b7a7d1f7b60e16c2836b61ff5516d7f8
all0neee <Andaev.1@yandex.ru>
2025-04-28 14:59:26 +0300

    feat: add logging to bot loader file

--- bot/loader.py
+++ bot/loader.py
@@ -36,17 +36,25 @@ BOT_TOKEN = os.getenv("BOT_TOKEN")
 if BOT_TOKEN is None:
     logger.error("Can't find BOT_TOKEN env variable")
     sys.exit()
-    
+
 bot = Bot(BOT_TOKEN, parse_mode="HTML")
-redis_host = os.getenv("REDIS_HOST", "localhost")
-redis_port = 6379
-redis_db = 1
-storage = RedisStorage2(host=redis_host, port=redis_port, db=redis_db, prefix='fsm', data_ttl=1800, state_ttl=1800, pool_size=100)
-dp = Dispatcher(bot=bot, storage=storage)
+try:
+    redis_host = os.getenv("REDIS_HOST", "localhost")
+    redis_port = 6379
+    redis_db = 1
+    storage = RedisStorage2(host=redis_host, port=redis_port, db=redis_db, prefix='fsm', data_ttl=1800, state_ttl=1800,
+                            pool_size=100)
+    dp = Dispatcher(bot=bot, storage=storage)
+    logger.info("Redis connected. Dispatcher initialized successfully.")
+except Exception as e:
+    logger.error(f"No connection to redis. Error: {e}")
+
+
 
 ADMINS_IDS = os.getenv("ADMINS_IDS")
 admins = []
-if ADMINS_IDS is not None:    
+if ADMINS_IDS is not None:
+    logger.info("Admins found. Parse admins IDs")
     admins = ADMINS_IDS.split(",")
     admins = [admin for admin in admins if admin != ""]
 
@@ -56,5 +64,7 @@ while django_start is False:
     try:
         token = get_token()
         django_start = True
-    except Exception:
+        logger.info("Successfully connected to Django and received token.")
+    except Exception as e:
+        logger.warning(f"Waiting for Django to start... Error: {e}")
         time.sleep(3)
